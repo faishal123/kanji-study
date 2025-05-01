5a65ffe4ec943c57de98aca12a480483b4cdc7b1
Faishal <faisal@tantowi.com>
2025-05-01 23:46:37 +0700

    fix: remove any

--- src/utils/fetchKanji.ts
+++ src/utils/fetchKanji.ts
@@ -13,19 +13,3 @@ export const fetchKanjiList = async (
     console.error("Error fetching kanji list:", error);
   }
 };
-
-export const fetchKanjiDetails = async (
-  kanji: string,
-  onComplete: (data: Record<string, any>) => void
-) => {
-  try {
-    const response = await fetch(`https://kanjiapi.dev/v1/kanji/${kanji}`);
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
-    }
-    const data = await response.json();
-    onComplete(data);
-  } catch (error) {
-    console.error("Error fetching kanji details:", error);
-  }
-};
